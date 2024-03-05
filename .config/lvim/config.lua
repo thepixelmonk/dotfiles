dbbaf9b825dd53df58b99a182e2d5f0a2e46a095
Chris Bolton <hello@bolton.dev>
2024-03-04 18:27:59 -0800

    lunarvim

---
+++ .config/lvim/config.lua
@@ -0,0 +1,77 @@
+local bufopts = { noremap = true, silent = true }
+local gpt = {
+  openai_params = {
+    model = "gpt-4",
+  },
+  openai_edit_params = {
+    model = "gpt-4",
+  },
+}
+
+function FocusFirstFloat()
+    local windows = vim.api.nvim_list_wins()
+    for _, win in ipairs(windows) do
+        local config = vim.api.nvim_win_get_config(win)
+        if config.focusable and config.relative ~= "" then
+            vim.api.nvim_set_current_win(win) -- Focus the first floating window
+            return
+        end
+    end
+end
+
+function OpenFloatDiagnostic()
+    local diagnostics = vim.diagnostic.get(0)
+
+    if next(diagnostics) == nil then
+        print("No diagnostics available")
+        return
+    end
+
+    vim.diagnostic.open_float(0, { scope = 'line' })
+end
+
+vim.opt.shiftwidth = 4
+vim.g.astro_stylus = "enable"
+vim.g.astro_typescript = "enable"
+
+vim.api.nvim_set_keymap('n', '<c-w><space>', ':lua FocusFirstFloat()<cr>', bufopts)
+
+lvim.plugins = {
+  {
+    "ThePrimeagen/harpoon",
+      branch = "harpoon2", -- Specify the branch here
+      config = function()
+        require("harpoon").setup({})
+    end
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+      event = "VeryLazy",
+      config = function()
+        require("chatgpt").setup(gpt)
+      end,
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "folke/trouble.nvim",
+        "nvim-telescope/telescope.nvim"
+      }
+  }
+}
+
+lvim.builtin.which_key.mappings["t"] = { "<cmd>ToggleTerm<cr>", "Terminal" }
+lvim.builtin.which_key.mappings["d"] = { function() OpenFloatDiagnostic() end, "Diagnostics" }
+
+lvim.builtin.which_key.mappings["a"] = {
+  name = "+AI",
+  c = { ":ChatGPT<cr>", "Chat" },
+  e = { ":ChatGPTEditWithInstructions<cr>", "Edit" },
+}
+
+lvim.builtin.which_key.mappings["h"] = {
+  name = "+Harpoon",
+  a = { ":lua require('harpoon'):list():append()<cr>", "Append" },
+  m = { ":Telescope harpoon marks<cr>", "Marks" },
+  s = { ":lua require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())<cr>", "Scratchpad" },
+}
+
